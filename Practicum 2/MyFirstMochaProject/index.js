ac62d9b4ad171620d596793277e2dc2498e3be43
Twan van Maastricht <ajmaastr@avans.nl>
2017-05-04 12:06:30 +0200

    add gitignore en webport voor server

--- Practicum 2/MyFirstMochaProject/index.js
+++ Practicum 2/MyFirstMochaProject/index.js
@@ -4,12 +4,25 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var config = require('./config.json');
+
+app.set("PORT", config.webPort);
+var port = process.env.PORT || app.get('PORT');
 
 app.get("/about", function (req, res) {
     res.json({
         "Info": "Dit is een simpele server"
     })
+});
+
+app.get('/', function(request, response) {
+    response.send('Hello Avans!');
+})
+
+app.get("*", function (res, req) {
+    res.status(404);
+    res.send("404- Not Found");
+
 });
 
 app.listen(port, function () {
